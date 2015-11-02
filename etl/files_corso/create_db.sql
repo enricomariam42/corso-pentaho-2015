e97fe233f5b205d461bd7204bf2cec7e591518bc
Enrico Maria Carmona <e.carmona@asst-monza.it>
2015-11-02 15:28:54 +0100

    dim_presidio: trasformazione semplificata create_db.sql: commentato user utente_corso         dim_presidio: aggiunto script insert record 0 e modificato script creazione

--- etl/files_corso/create_db.sql
+++ etl/files_corso/create_db.sql
@@ -1,45 +1,24 @@
 DROP database IF EXISTS corso ;
 CREATE database corso CHARACTER SET utf8 ;
-CREATE USER 'utente_corso'@'%' IDENTIFIED BY 'password' ;
-GRANT ALL ON `corso`.* TO 'utente_corso'@'%' ;
-FLUSH PRIVILEGES ;
+-- CREATE USER 'utente_corso'@'%' IDENTIFIED BY 'password' ;
+-- GRANT ALL ON `corso`.* TO 'utente_corso'@'%' ;
+-- FLUSH PRIVILEGES ;
 
 use corso;
 
-CREATE TABLE IF NOT EXISTS dim_presidio
-(
-  id INT(11) NOT NULL PRIMARY KEY
-, ente VARCHAR(8)
-, presidio VARCHAR(8)
-, descrizione VARCHAR(256)
-, version INT(11)
-, date_from DATETIME
-, date_to DATETIME
-)
-;CREATE INDEX idx_dim_presidio_lookup ON dim_presidio(ente, presidio)
-;
-CREATE INDEX idx_dim_presidio_tk ON dim_presidio(id)
-;
+CREATE TABLE `dim_presidio` (
+  `id` int(11) NOT NULL,
+  `ente` varchar(16) DEFAULT NULL,
+  `presidio` varchar(16) DEFAULT NULL,
+  `descrizione` varchar(128) DEFAULT NULL,
+  `version` int(11) DEFAULT NULL,
+  `date_from` date DEFAULT NULL,
+  `date_to` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_dim_presidio_lookup` (`ente`,`presidio`),
+  KEY `idx_dim_presidio_tk` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ;
 
-CREATE TABLE IF NOT EXISTS dim_data
-(
-  id INT not null
-, date_field DATETIME
-, year INT
-, month INT
-, day_of_year INT
-, day_of_month INT
-, day_of_week INT
-, week_of_year INT
-, day_of_week_desc VARCHAR(30)
-, day_of_week_short_desc VARCHAR(3)
-, month_desc VARCHAR(30)
-, month_short_desc VARCHAR(3)
-, quarter VARCHAR(15)
-, PRIMARY KEY (`id`)
-, KEY `idx_DIM_DATE_lookup` (`date_field`)
-, KEY `idx_DIM_DATE_tk` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=UTF8
-;
-
+INSERT INTO dim_presidio (id,ente,presidio,descrizione,version,date_from,date_to) 
+        VALUES (0 /*not nullable*/,'Non Disponibile','Non Disponibile','Non Disponibile',1,{d '1900-01-01'},{d '2200-01-01'});
 
